5b0f13a0c5b17a91783a4286eda6448d877b368e
Metehan Ayhan <161254242+metehanayhan@users.noreply.github.com>
2024-03-08 16:26:42 +0300

    Update App.js

--- Metehan_Ayhan_Homework2/my-app/src/App.js
+++ Metehan_Ayhan_Homework2/my-app/src/App.js
@@ -5,16 +5,32 @@ import UserList from "./Components/UserList";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
   const addUser = (user) => {
     const newUser = { ...user, id: new Date().getTime() };
     setUsers([...users, newUser]);
+    setShowForm(false);
+  };
+
+  const handleCancel = () => {
+    setShowForm(false);
   };
 
   return (
     <div className="container">
       <h1>User Addition Menu</h1>
-      <UserForm addUser={addUser} />
+      <button onClick={() => setShowForm(true)}>Add User</button>
+      {showForm && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleCancel}>
+              &times;
+            </span>
+            <UserForm addUser={addUser} onCancel={handleCancel} />
+          </div>
+        </div>
+      )}
       <UserList users={users} />
     </div>
   );
